eb0d497c98fad41de263ef87a5c243a06b528e24
Mohit Joping <mohitjoping1@gmail.com>
2024-09-09 02:51:04 +0530

    Initialize frontend app and setup file structure

---
+++ app/(routes)/(home)/(routes)/home/page.tsx
@@ -0,0 +1,15 @@
+// tsrfc
+
+import React from "react";
+import { ChatBot } from "../../../../../../components/Organisms/chatbot";
+
+type Props = {};
+
+export default function page({}: Props) {
+  return (
+    <>
+      <ChatBot />
+      {/* here you can render your components like we render in react */}
+    </>
+  );
+}
---
+++ components/Organisms/chatbot.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export function ChatBot({}: Props) {
+  return (
+<div>chatbot</div>
+  )
+}
